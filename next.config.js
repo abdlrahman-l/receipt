a073edc47a3161380c4e54ab8c20fd40a4bb41c1
Abdul Rahman <abdul.rahman@happyfresh.com>
2022-09-27 00:57:58 +0700

    transpile modules and breakpoints update version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withTM = require('next-transpile-modules')(['@manskuy/breakpoints']);
+const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['www.themealdb.com'],
   }
-}
+})
 
 module.exports = nextConfig
